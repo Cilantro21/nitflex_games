037c6ceb5c86ac7ee01ccdb9434bf189c96f5fc2
Cilantro21 <aleamundo@hotmail.com>
2023-08-26 17:54:51 -0600

    Pending Vendor Service

---
+++ webapi/Interfaces/IVendorService.cs
@@ -0,0 +1,13 @@
+﻿using webapi.Models;
+
+namespace webapi.Interfaces
+{
+    public interface IVendorService
+    {
+        Task<Vendor> GetAllVendors();
+        Task<Vendor> GetVendorById(int id);
+        Task AddVendor(Vendor vendor);
+        Task UpdateVendor(Vendor vendor, int id);
+        Task DeleteVendor(int id);
+    }
+}
---
+++ webapi/Services/VendorsService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Interfaces;
+using webapi.Models;
+
+namespace webapi.Services
+{
+    public class VendorsService : IVendorService
+    {
+        private readonly AppDbContext _context;
+
+        public VendorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Vendor>> GetAllVendors()
+        {
+            return await _context.Vendors.ToListAsync();
+        }
+
+        public async Task<Vendor> GetVendorById(int id)
+        {
+            var vendor = await _context.Vendors.FindAsync(id);
+            return vendor;
+        }
+
+        public async Task AddVendor(Vendor vendor)
+        {
+            await _context.Vendors.AddAsync(vendor);
+            _context.SaveChanges();
+        }
+    }
+}
